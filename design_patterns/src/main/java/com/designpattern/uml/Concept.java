073558a93815848cf324f7ec91f2e74bd33735b1
keke0007 <jiangke12@yeah.net>
2020-07-24 16:42:16 +0800

    uml

--- design_patterns/src/main/java/com/designpattern/uml/Concept.java
+++ design_patterns/src/main/java/com/designpattern/uml/Concept.java
@@ -6,4 +6,11 @@ package com.designpattern.uml;
  * @create 2020/7/24 11:45
  */
 public class Concept {
+
+    /**
+     *  UML(Unified Modeling Language,统一建模语言)是用于面向对象分析和设计的表示方式
+     *
+     *
+     *
+     */
 }
