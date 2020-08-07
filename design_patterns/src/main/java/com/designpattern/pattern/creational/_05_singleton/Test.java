bd1304979db1988869dbfe3b53054af087ec9373
keke0007 <jiangke12@yeah.net>
2020-08-07 16:38:08 +0800

    add singleton

---
+++ design_patterns/src/main/java/com/designpattern/pattern/creational/_05_singleton/Test.java
@@ -0,0 +1,16 @@
+package com.designpattern.pattern.creational._05_singleton;
+
+/**
+ * @author jk
+ * @version 1.0.0
+ * @create 2020/8/7 9:51
+ */
+public class Test {
+    public static void main(String[] args) {
+        short s1 = 1;
+        // s1 = s1 + 1;
+        s1 += 1;
+        System.out.println(s1);
+        System.out.println(42 == 42.0);
+    }
+}
